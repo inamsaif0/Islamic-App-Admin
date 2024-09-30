81c006f815551f668772d243673aa8325a462ab3
Inam Ullah Yousufzai <58142833+inamsaif0@users.noreply.github.com>
2024-09-30 20:17:03 +0500

    Update Baseurl.js

--- src/Baseurl/Baseurl.js
+++ src/Baseurl/Baseurl.js
@@ -4,7 +4,7 @@ export default {
     
 
     
-    baseUrl:"http://5.104.83.184:9000/",
+    baseUrl:"https://api.ooridoo.com/",
     // baseUrl:"https://pyurelyecommerce.pythonanywhere.com/api",
     // http://5.104.83.184:9000/
     // imgUrl:"https://pyurelyecommerce.pythonanywhere.com/media/"
